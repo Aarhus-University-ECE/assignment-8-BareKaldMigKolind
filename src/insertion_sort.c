5fee0fcbef3ecafb372d8dc5e658d55c9b87cbfe
BareKaldMigKolind <madsskolind@gmail.com>
2022-10-27 14:10:41 +0200

    TestCommit

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -1,9 +1,32 @@
 #include "insertion_sort.h"
+
 #include "linked_list.h"
 
+void sort(linked_list* llPtr) {
+    // Add your sort function here
 
+    linked_list* sortedList = createLinkedList();
 
-void sort(linked_list *llPtr)
-{
- // Add your sort function here
+    node_t* currentUnsorted = llPtr->head;
+    node_t* currentSorted;
+
+    while (currentUnsorted != NULL) {
+        currentSorted = createNode(currentUnsorted->data);
+        insertFront(currentSorted, sortedList);
+
+        while (currentSorted->data > currentSorted->next->data &&
+               currentSorted->next != NULL) {
+            int tempVal = currentSorted->data;
+            currentSorted->data = currentSorted->next->data;
+            currentSorted->next->data = tempVal;
+
+            currentSorted = currentSorted->next;
+        }
+
+        currentUnsorted = currentUnsorted->next;
+    }
+
+    llPtr = sortedList;
+
+    return;
 }
