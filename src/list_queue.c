e5703c4eff77fc914d0deda4a76efa63294d1aca
BareKaldMigKolind <madsskolind@gmail.com>
2022-10-31 16:46:49 +0100

    Aflevering Uge 8 DONE

--- src/list_queue.c
+++ src/list_queue.c
@@ -1,22 +1,86 @@
 #include "list_queue.h"
 
+#include <stdio.h>
 
-void init_queue(queue *q)
-{
-  // Add your init_queue
+#include "assert.h"
+
+void init_queue(queue *q) {
+    // Add your init_queue
+    // Laver en tom queue. Derfor skal front og rear være NULL.
+    q->front = NULL;
+    q->rear = NULL;
+
+    // Størrelsen er naturligvis 0.
+    q->size = 0;
 }
 
-int empty(queue *q)
-{
-  // Add your empty function  
+int empty(queue *q) {
+    // Add your empty function
+    // I empty kigger jeg bare på størrelsen af queuen. Altså om size == 0.
+    // Jeg returner 1 hvis size == 0, fordi empty == true.
+    if (q->size == 0) {
+        return 1;
+    }
+    // Ellers er empty == false og derfor returner jeg 0.
+    return 0;
 }
 
-void enqueue(queue *q, int x)
-{
-  // Add your enqueue function
+void enqueue(queue *q, int x) {
+    // Add your enqueue function
+    // Først skal der gøres plads til en ny qnode. Derfor bruger jeg malloc. Jeg
+    // kalder den nye qnode for RearNew.
+    qnode *rearNew = (qnode *)malloc(sizeof(qnode));
+
+    // Nu skal data og pointer opdateres. Da det er et rear element, skal
+    // rearNew pege på NULL.
+
+    rearNew->data = x;
+    rearNew->next = NULL;
+
+    // Nu skal rearNew indsættes i queuen q. Der er to scenarier, enten er q tom
+    // eller også er den ikke. Derfor tjekker jeg om q er tom:
+    if (empty(q) == 1) {
+        // Hvis q er tom, så er rearNew både forreste og bagerste element.
+        // Derfor skal den både hver front og rear.
+        q->front = rearNew;
+    } else {
+        // Hvis q ikke er tom, så skal q->rear ikke længere pege på NULL, men på
+        // rearNew.
+        q->rear->next = rearNew;
+    }
+    // Da vi har ændret q->rear og fået et nyt element bagerst, så skal q-rear
+    // opdateres.
+    q->rear = rearNew;
+
+    // Desuden er størrelsen af q nu vokset med en.
+    q->size = q->size + 1;
 }
 
-int dequeue(queue *q)
-{
-  // Add your dequeue function
+int dequeue(queue *q) {
+    // Add your dequeue function
+
+    // Først tjekker jeg lige at queue q ikke er tom. Det giver nemlig ikke
+    // mening at dequeue på en tom queue.
+    assert(empty(q) == 0);
+
+    // Dequeue skal returne data fra den node, som slettes. Derfor skal vi gemme
+    // dataen. Det bruger jeg temp til.
+    int temp = q->front->data;
+
+    // Før jeg opdaterer queuens front, gemmer jeg den først i en pointer kaldet
+    // formerFront
+    qnode *formerFront = q->front;
+
+    // Så opdaterer jeg fronten på queuen til at være det næste elementen i
+    // queuen.
+    q->front = q->front->next;
+
+    // Og jeg husker at free formerFront, så vi undergår memory leak.
+    free(formerFront);
+
+    // Nu er q->size blevet en mindre, derfor skal size opdateres.
+    q->size = q->size - 1;
+
+    // Jeg returner temp.
+    return temp;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,57 @@
-extern "C"{
-    // Add the header files required to run your main 
+extern "C" {
+// Add the header files required to run your main
+#include "insertion_sort.h"
+#include "linked_list.h"
+#include "list_queue.h"
+}
+
+void testFunction() {
+    queue q;
+    int x = 5;
+    int x0 = 10;
+    int x1 = 20;
+    int y;
+    int y0;
+    int y1;
+
+    // Test A
+    printf("Test A: \n");
+    init_queue(&q);
+    if (empty(&q) == 1) {
+        printf("Test A == Succes\n\n");
+    } else {
+        printf("Something went wrong with Test A\n\n");
+        abort;
+    }
+
+    // Test B
+    printf("\nTest B:\n");
+    enqueue(&q, x);
+    y = dequeue(&q);
+    if (x == y && empty(&q) == 1) {
+        printf("Test B == Success \n\n");
+    } else {
+        printf("Something went wrong with Test B\n\n");
+        abort;
+    }
+
+    // Test C
+    printf("\nTest C:\n");
+    enqueue(&q, x0);
+    enqueue(&q, x1);
+    y0 = dequeue(&q);
+    y1 = dequeue(&q);
+    if (x0 == y0 && x1 == y1 && empty(&q)) {
+        printf("Test C == Success \n\n");
+    } else {
+        printf("Something went wrong with Test C\n\n");
+        abort;
+    }
 }
 
 // File for sandboxing and trying out code
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+    testFunction();
+
     return 0;
 }
